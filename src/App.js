cfae3873bf9e09ecf04ea926195f053cbb9d2a1e
RyanLinrm <runminlin@gmail.com>
2020-10-15 21:32:19 -0400

    added homepage with buttons

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import './App.css';
-import YouTube from 'react-youtube';
-import Cookies from 'js-cookie';
-import QRCode from 'qrcode.react'
-import * as mm from "@magenta/music";
-import MagentaCom from './components/mm.js';
-import Generator from './components/generator';
-import MidiKeyboard from './components/piano';
-import WebPiano from './components/RecordingPiano';
-import RNN from './components/MusicRNN';
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import Homepage from "./pages/homepage"
+
 
 class App extends React.Component {
-  
   render(){
-
     return (
-      <div className="App">
-        <h4 className="text-center">Piano Roll:</h4>
-        <MagentaCom />
-        <br/>
-        <Generator />
-        <br/>
-        {/*<MidiKeyboard />*/}
-        <WebPiano />
-        <br/>
-        <RNN />
-      </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/">
+              <Homepage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
-
-}
+  }
 }
 
 export default App;
---
+++ src/pages/homepage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <br/>
+                <h1 className="text-center">Home Page</h1>
+                <h3 className='text-center'>Artificial Music Generator</h3>
+                <br/><br/><br/>
+                <div className='text-center'>
+                    <button className='btn btn-primary'>MusicVAE</button>
+                    <br/><br/>
+                    <button className='btn btn-danger'>MusicRNN</button>
+                    <br/><br/>
+                    <button className='btn btn-info'>About</button>
+                    <br/><br/>
+                    <button className='btn'>Login</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Homepage
